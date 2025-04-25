e3776d244b5397b4a886e46d8fa07c5dad6a1eda
Diegovsky (Diego Augusto) <diegocastro169169@gmail.com>
2025-04-25 18:09:29 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="bg-sky-500">Negro até os ossos</h1>
+    </>
+  )
+}
+
+export default App
