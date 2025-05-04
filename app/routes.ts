182fd1359ccd72c2f6ead4d1a993d36169d4fbc2
Diegovsky (Diego Augusto) <diegocastro169169@gmail.com>
2025-05-03 21:47:16 -0300

    Move to React Router

---
+++ app/routes.ts
@@ -0,0 +1,10 @@
+import { type RouteConfig, route } from "@react-router/dev/routes";
+// import { flatRoutes } from "@react-router/fs-routes";
+
+export default [
+  route("/", "./home.tsx"),
+  route("/about-us", "./about-us.tsx"),
+
+  // ...(await flatRoutes()),
+] satisfies RouteConfig;
+
