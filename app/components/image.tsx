e4a7daef047305ac46de1aba51cbb941178f05a8
Diegovsky (Diego Augusto) <diegocastro169169@gmail.com>
2025-07-03 14:01:32 -0300

    Custom

--- app/components/image.tsx
+++ app/components/image.tsx
@@ -1,15 +1,23 @@
 import type { ReactElement } from "react";
 import * as api from "~/api";
+import { cn } from "~/lib/utils";
 
 export default function Image({
 	name,
 	url,
+	className,
 }: {
 	name: string;
 	url: string;
+	className: string;
 }): ReactElement {
 	return (
-		<div className="flex flex-col text-zinc-100 space-y-2 items-center">
+		<div
+			className={cn(
+				"flex flex-col text-zinc-100 space-y-2 items-center",
+				className,
+			)}
+		>
 			<div className="size-16">
 				<img className="w-full object-fill" src={url} aria-label="Whoa" />
 			</div>
